18e64b2314a7984e2f6e126ee2d402b4c2a0eb50
tommu <tom.muet@orange.fr>
2023-01-17 14:41:26 +0100

    ajout trainer

---
+++ WWA-Trainer/src/test/java/com/WWATrainer/WWATrainer/WwaTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WWATrainer.WWATrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwaTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
