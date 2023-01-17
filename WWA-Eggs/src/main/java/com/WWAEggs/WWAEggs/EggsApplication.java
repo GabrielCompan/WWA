c709cf798c900e8ac54c0205238bea927d70f17d
GabrielCompan <gabrielcompanpro@gmail.com>
2023-01-17 11:06:18 +0100

    Initializing Project

---
+++ WWA-Eggs/src/main/java/com/WWAEggs/WWAEggs/EggsApplication.java
@@ -0,0 +1,13 @@
+package com.WWAEggs.WWAEggs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwaEggsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwaEggsApplication.class, args);
+	}
+
+}
---
+++ WWA-Eggs/src/test/java/com/WWAEggs/WWAEggs/WwaEggsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WWAEggs.WWAEggs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwaEggsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
