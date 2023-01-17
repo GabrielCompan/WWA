4337df244eb4322f2408cc505d305c71f53d6dab
GabrielCompan <gabrielcompanpro@gmail.com>
2023-01-17 11:09:01 +0100

    Initializing Project

--- WWA-Eggs/src/main/java/com/WWAEggs/WWAEggs/EggsServiceApplication.java
+++ WWA-Eggs/src/main/java/com/WWAEggs/WWAEggs/EggsServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WwaEggsApplication {
-
+public class EggsServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(WwaEggsApplication.class, args);
+		SpringApplication.run(EggsServiceApplication.class, args);
 	}
-
 }
