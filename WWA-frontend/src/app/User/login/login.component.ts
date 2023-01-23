e9262032d301d89cafbff60f9fd2a11fd1c4947d
NicolasPois01 <pois.nicolas01@gmail.com>
2023-01-23 10:13:35 +0100

    models

--- WWA-frontend/src/app/User/login/login.component.ts
+++ WWA-frontend/src/app/User/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../../Services/Auth/auth.service';
 import {Router} from '@angular/router';
 
