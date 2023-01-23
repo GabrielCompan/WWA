ea30504af32aeb164c55261b953ea66d818f6c5c
tommu <tom.muet@orange.fr>
2023-01-23 16:36:13 +0100

    prout

---
+++ WWA-Hatching/src/test/java/com/hatching/hatching/IncubatorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.hatching.hatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncubatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
