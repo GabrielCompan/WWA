3f4d577198e4b3b1b8746780ef02c77782a33317
GabrielCompan <gabrielcompanpro@gmail.com>
2023-01-24 10:18:05 +0100

    Create "Eggs" Class

---
+++ WWA-Eggs/src/main/java/com/eggs/eggs/model/Eggs.java
@@ -0,0 +1,44 @@
+package com.eggs.eggs.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Eggs {
+    private int id;
+    private String name;
+    private double price;
+    private int hatchingTime;
+
+    public Eggs(int id, String name, double price, int hatchingTime) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.hatchingTime = hatchingTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getHatchingTime() {
+        return hatchingTime;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public void setHatchingTime(int hatchingTime) {
+        this.hatchingTime = hatchingTime;
+    }
+
+}
--- WWA-Eggs/src/main/java/com/eggs/eggs/web/controller/EggsController.java
+++ WWA-Eggs/src/main/java/com/eggs/eggs/web/controller/EggsController.java
@@ -1,4 +1,4 @@
-package com.WWAEggs.WWAEggsService.web.controller;
+package com.eggs.eggs.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.eggs.eggs.repository.EggsRepository;
+import com.eggs.eggs.model.Eggs;
+import com.eggs.eggs.repository.EggsRepository;
 
 import java.sql.Array;
 import java.util.List;
