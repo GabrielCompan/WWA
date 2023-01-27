c89e49d7740da8693f108f771537ea9b89ce2f38
NicolasPois01 <pois.nicolas01@gmail.com>
2023-01-27 14:33:12 +0100

    Test Trainer

--- WWA-frontend/src/app/trainer/trainer.component.ts
+++ WWA-frontend/src/app/trainer/trainer.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TrainerService} from '../Services/Trainer/trainer.service';
+import {Trainer} from '../model/trainer';
 
 @Component({
   selector: 'app-trainer',
   templateUrl: './trainer.component.html',
   styleUrls: ['./trainer.component.scss']
 })
-export class TrainerComponent {
+export class TrainerComponent implements OnInit {
+
+  trainer!: Trainer;
+
+  constructor(private trainerService: TrainerService) { }
+
+  ngOnInit() {
+    this.getTrainerById(1)
+    if (this.trainer == null) {
+      this.trainer = {
+        id: 1,
+        name: 'Ash',
+        money: 0,
+      }
+    }
+  }
+
+  getTrainerById(id: number) {
+    this.trainerService.getTrainerById(id).subscribe((data: Trainer) => {this.trainer = data});
+  }
+
+  addPokeDollars(amount: number) {
+
+  }
 
 }
